572fa74659cf209691451a64311ab98156448349
Nathan Rajlich <nathan@tootallnate.net>
2013-12-07 13:33:16 -0800

    dnsDomainIs: add examples to jsdocs

--- dnsDomainIs.js
+++ dnsDomainIs.js
@@ -8,6 +8,20 @@ module.exports = dnsDomainIs;
 /**
  * Returns true iff the domain of hostname matches.
  *
+ * Examples:
+ *
+ * ``` js
+ * dnsDomainIs("www.netscape.com", ".netscape.com")
+ *   // is true.
+ *
+ * dnsDomainIs("www", ".netscape.com")
+ *   // is false.
+ *
+ * dnsDomainIs("www.mcom.com", ".netscape.com")
+ *   // is false.
+ * ```
+ *
+ *
  * @param {String} host is the hostname from the URL.
  * @param {String} domain is the domain name to test the hostname against.
  * @return {Boolean} true iff the domain of the hostname matches.
