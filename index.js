1d9835f6494b759c8b7ac08e2f617fc32424b569
Nathan Rajlich <nathan@tootallnate.net>
2013-12-06 17:44:01 -0800

    use `degenerator` to compile the PAC proxy file

--- index.js
+++ index.js
@@ -5,8 +5,33 @@
 
 var co = require('co');
 var vm = require('vm');
-var compile = require('./compile');
+var degenerator = require('degenerator');
+var regenerator = require('regenerator');
+
+/**
+ * Built-in PAC functions.
+ */
+
 var dnsDomainIs = require('./dnsDomainIs');
+
+/**
+ * `names` array for `degenerator`.
+ */
+
+var builtIns = [
+  'isPlainHostName',
+  'dnsDomainIs',
+  'localHostOrDomainIs',
+  'isResolvable',
+  'isInNet',
+  'dnsResolve',
+  'myIpAddress',
+  'dnsDomainLevels',
+  'shExpMatch',
+  'weekdayRange',
+  'dateRange',
+  'timeRange'
+];
 
 /**
  * Module exports.
@@ -26,14 +51,16 @@ function generate (str) {
 
   // convert the JS FindProxyForURL function into
   // a generator function
-  var js = compile(str);
+  var js = degenerator(str, builtIns);
+
+  // use `facebook/regnerator` for node < v0.11 support
+  // TODO: don't use regenerator if native generators are supported...
+  js = regenerator(js, { includeRuntime: true });
 
   // the sandbox to use for the vm
   // TODO: make configurable
   var sandbox = {
-    dnsDomainIs: dnsDomainIs,
-    animal: 'cat',
-    count: 2
+    dnsDomainIs: dnsDomainIs
   };
 
   // filename of the pac file for the vm
@@ -41,13 +68,18 @@ function generate (str) {
   var filename = 'proxy.pac';
 
   // evaluate the JS string and extract the FindProxyForURL generator function
-  var FindProxyForURL = vm.runInNewContext(js + ';FindProxyForURL', sandbox, filename);
+  var fn = vm.runInNewContext(js + ';FindProxyForURL', sandbox, filename);
+  if ('function' != typeof fn) {
+    throw new TypeError('PAC file JavaScript contents must define a `FindProxyForURL` function');
+  }
 
   // return the async resolver function
-  return function (url, host, callback) {
-    co(function* () {
-      var res = yield FindProxyForURL(url, host);
-      return res || 'DIRECT';
-    })(callback);
+  var resolver = co(fn);
+
+  return function FindProxyForURL (url, host, fn) {
+    resolver(function (err, res) {
+      if (err) return fn(err);
+      fn(null, res || 'DIRECT');
+    });
   };
 }
