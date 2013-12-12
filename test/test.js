596f4c3de2179c8b3edbb8e78319ed3b4e824e6c
Nathan Rajlich <nathan@tootallnate.net>
2013-12-11 20:30:25 -0800

    test: add "official docs Example #1b tests

--- test/test.js
+++ test/test.js
@@ -61,8 +61,56 @@ describe('FindProxyForURL', function () {
       });
     });
 
-    it('should return "PROXY …" for "foo.netscape.com"', function (done) {
+    it('should return "PROXY …" for "google.com"', function (done) {
       FindProxyForURL('http://google.com/t', 'google.com', function (err, res) {
+        if (err) return done(err);
+        assert.equal('PROXY w3proxy.netscape.com:8080; DIRECT', res);
+        done();
+      });
+    });
+
+  });
+
+  describe('offical docs Example #1b', function () {
+    var FindProxyForURL = pac(
+      'function FindProxyForURL(url, host)' +
+      '{' +
+      '    if ((isPlainHostName(host) ||' +
+      '         dnsDomainIs(host, ".netscape.com")) &&' +
+      '        !localHostOrDomainIs(host, "www.netscape.com") &&' +
+      '        !localHostOrDomainIs(host, "merchant.netscape.com"))' +
+      '        return "DIRECT";' +
+      '    else' +
+      '        return "PROXY w3proxy.netscape.com:8080; DIRECT";' +
+      '}'
+    );
+
+    it('should return "DIRECT" for "localhost"', function (done) {
+      FindProxyForURL('http://localhost/hello', 'localhost', function (err, res) {
+        if (err) return done(err);
+        assert.equal('DIRECT', res);
+        done();
+      });
+    });
+
+    it('should return "DIRECT" for "foo.netscape.com"', function (done) {
+      FindProxyForURL('http://foo.netscape.com/', 'foo.netscape.com', function (err, res) {
+        if (err) return done(err);
+        assert.equal('DIRECT', res);
+        done();
+      });
+    });
+
+    it('should return "PROXY …" for "www.netscape.com"', function (done) {
+      FindProxyForURL('http://www.netscape.com/', 'www.netscape.com', function (err, res) {
+        if (err) return done(err);
+        assert.equal('PROXY w3proxy.netscape.com:8080; DIRECT', res);
+        done();
+      });
+    });
+
+    it('should return "PROXY …" for "merchant.netscape.com"', function (done) {
+      FindProxyForURL('http://merchant.netscape.com/', 'merchant.netscape.com', function (err, res) {
         if (err) return done(err);
         assert.equal('PROXY w3proxy.netscape.com:8080; DIRECT', res);
         done();
