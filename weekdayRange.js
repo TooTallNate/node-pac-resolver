a93a815f0d2538aac1957c46bf1ecc77e86b6a64
Utsav Kesharwani <utsav.kesharwani@gmail.com>
2016-09-26 01:13:34 +0530

    Implemented weekdayRange function

--- weekdayRange.js
+++ weekdayRange.js
@@ -48,7 +48,33 @@ module.exports = weekdayRange;
  * @return {Boolean}
  */
 
+const dayOrder = { "SUN": 0, "MON": 1, "TUE": 2, "WED": 3, "THU": 4, "FRI": 5, "SAT": 6 };
+
 function weekdayRange (wd1, wd2, gmt) {
-  // TODO: implement me!
-  return false;
+
+  var useGMTzone = (wd2 == "GMT" || gmt == "GMT"),
+      todaysDay  = getTodaysDay(useGMTzone),
+      wd1Index   = dayOrder[wd1] || -1,
+      wd2Index   = dayOrder[wd2] || -1,
+      result     = false;
+
+  if (wd2Index < 0) {
+    result = (todaysDay == wd1Index);
+  } else {
+    if (wd1Index <= wd2Index) {
+      result = valueInRange(wd1Index, todaysDay, wd2Index);
+    } else {
+      result = valueInRange(wd1Index, todaysDay, 6) || valueInRange(0, todaysDay, wd2Index);
+    }
+  }
+  return result;
+}
+
+function getTodaysDay (gmt) {
+  return (gmt ? (new Date()).getUTCDay() : (new Date()).getDay());
+}
+
+// start <= value <= finish
+function valueInRange (start, value, finish) {
+  return (start <= value) && (value <= finish);
 }
