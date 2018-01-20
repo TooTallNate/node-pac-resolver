5f12ee29471ece7d220933011623c0d1204e60a2
Jason <jason.daurus@gmail.com>
2018-01-21 05:26:30 +0800

    Escape regex characters in shExpMatch (#17)

--- test/shExpMatch.js
+++ test/shExpMatch.js
@@ -11,13 +11,13 @@ describe('shExpMatch(str, shexp)', function () {
   var tests = [
     ["http://home.netscape.com/people/ari/index.html", "*/ari/*", true],
     ["http://home.netscape.com/people/montulli/index.html", "*/ari/*", false],
-    ["http://home.example.com/people/index.html", ".*/people/.*", true],
     ["http://home.example.com/people/yourpage/index.html", ".*/mypage/.*", false],
     ["www.hotmail.com", "*hotmail.com*", true],
     ["phishing-scam.com?email=someone@hotmail.com", "*hotmail.com*", true],
     ["abcdomain.com", "(*.abcdomain.com|abcdomain.com)", true],
     ["foo.abcdomain.com", "(*.abcdomain.com|abcdomain.com)", true],
     ["abddomain.com", "(*.abcdomain.com|abcdomain.com)", false],
+    ["abcdomain.com", "*.n.com", false],
     ["a.com", "?.com", true],
     ["b.com", "?.com", true],
     ["ab.com", "?.com", false]
