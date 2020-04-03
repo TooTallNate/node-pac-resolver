a21dd926b0e065065c7061785874daebaf5592f6
SteffenRumpf <steffenrumpf@deutschebahn.com>
2020-04-03 08:35:18 +0200

    Differentiate between lookup and resolve

--- src/util.ts
+++ src/util.ts
@@ -1,10 +1,31 @@
+import { Resolver, LookupAddress, LookupOptions, lookup } from 'dns';
 import { GMT } from './index';
-import { LookupAddress, LookupOptions, lookup } from 'dns';
 
-export function dnsLookup(
+export function resolveDNS(
+	host: string
+): Promise<string[]> {
+	const resolver = new Resolver();
+	if(process.env.CUSTOM_DNS_SERVER) {
+		let currentDNSservers = resolver.getServers();
+		resolver.setServers([process.env.CUSTOM_DNS_SERVER, ...currentDNSservers]);
+	}
+
+	return new Promise((resolve, reject) => {
+		resolver.resolve4(host, (err, res) => {
+			if (err) {
+				reject(err);
+			} else {
+				resolve(res);
+			}
+		});
+	});
+}
+
+export function lookupDNS(
 	host: string,
 	opts: LookupOptions
 ): Promise<string | LookupAddress[]> {
+
 	return new Promise((resolve, reject) => {
 		lookup(host, opts, (err, res) => {
 			if (err) {
